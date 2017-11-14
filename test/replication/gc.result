opomuc/tarantool

commit ba09475f5f5518629c71c97993a0d80afd48a54e

replica: advance gc state only when xlog is closed
Advancing replica->gc on every status update is inefficient as gc can
only be invoked when we move to the next xlog file. Currently, it's
acceptable, because status is only updated once per second, but there's
no guarantee that it won't be updated say every millisecond in future,
in which case advancing replica->gc on every status update may become
too costly.

So introduce a trigger invoked every time an xlog is closed by
recover_remaining_wals() and use it in relay to send a special gc
message.

src/box/relay.cc
@@ -68,6 +68,18 @@ struct relay_status_msg {
 };
 
 /**
+ * Cbus message to update replica gc state in tx thread.
+ */
+struct relay_gc_msg {
+	/** Parent */
+	struct cmsg msg;
+	/** Relay instance */
+	struct relay *relay;
+	/** Vclock signature to advance to */
+	int64_t signature;
+};
+
+/**
  * Cbus message to notify tx thread that relay is stopping.
  */
 struct relay_exit_msg {
@@ -234,13 +246,40 @@ tx_status_update(struct cmsg *msg)
 {
 	struct relay_status_msg *status = (struct relay_status_msg *)msg;
 	vclock_copy(&status->relay->tx.vclock, &status->vclock);
-	gc_consumer_advance(status->relay->replica->gc,
-			    vclock_sum(&status->vclock));
 	static const struct cmsg_hop route[] = {
 		{relay_status_update, NULL}
 	};
 	cmsg_init(msg, route);
 	cpipe_push(&status->relay->relay_pipe, msg);
+}
+
+/**
+ * Update replica gc state in tx thread.
+ */
+static void
+tx_gc_advance(struct cmsg *msg)
+{
+	struct relay_gc_msg *m = (struct relay_gc_msg *)msg;
+	gc_consumer_advance(m->relay->replica->gc, m->signature);
+	free(m);
+}
+
+static void
+relay_on_close_log_f(struct trigger *trigger, void * /* event */)
+{
+	static const struct cmsg_hop route[] = {
+		{tx_gc_advance, NULL}
+	};
+	struct relay *relay = (struct relay *)trigger->data;
+	struct relay_gc_msg *m = (struct relay_gc_msg *)malloc(sizeof(*m));
+	if (m == NULL) {
+		say_warn("failed to allocate relay gc message");
+		return;
+	}
+	cmsg_init(&m->msg, route);
+	m->relay = relay;
+	m->signature = vclock_sum(&relay->r->vclock);
+	cpipe_push(&relay->tx_pipe, &m->msg);
 }
 
 static void
@@ -288,9 +327,17 @@ relay_subscribe_f(va_list ap)
 	cbus_endpoint_create(&relay->endpoint, cord_name(cord()),
 			     fiber_schedule_cb, fiber());
 	cpipe_create(&relay->tx_pipe, "tx");
-
-	/* Create a guard to detach the relay from cbus on exit */
-	auto cbus_guard = make_scoped_guard([&]{
+	/* Setup garbage collection trigger. */
+	struct trigger on_close_log = {
+		RLIST_LINK_INITIALIZER, relay_on_close_log_f, relay, NULL
+	};
+	trigger_add(&r->on_close_log, &on_close_log);
+	/*
+	 * Create a guard to detach the relay from cbus and
+	 * clear the garbage collection trigger on exit.
+	 */
+	auto guard = make_scoped_guard([&]{
+		trigger_clear(&on_close_log);
 		relay_cbus_detach(relay);
 	});
 	relay_set_cord_name(relay->io.fd);

src/box/recovery.cc
@@ -30,7 +30,9 @@
  */
 #include "recovery.h"
 
+#include "small/rlist.h"
 #include "scoped_guard.h"
+#include "trigger.h"
 #include "fiber.h"
 #include "xlog.h"
 #include "xrow.h"
@@ -131,6 +133,7 @@ recovery_new(const char *wal_dirname, bool force_recovery,
 	xdir_check_xc(&r->wal_dir);
 
 	r->watcher = NULL;
+	rlist_create(&r->on_close_log);
 
 	guard.is_active = false;
 	return r;
@@ -148,6 +151,7 @@ recovery_close_log(struct recovery *r)
 			 r->cursor.name);
 	}
 	xlog_cursor_close(&r->cursor, false);
+	trigger_run(&r->on_close_log, NULL);
 }
 
 void
@@ -155,6 +159,7 @@ recovery_delete(struct recovery *r)
 {
 	recovery_stop_local(r);
 
+	trigger_destroy(&r->on_close_log);
 	xdir_destroy(&r->wal_dir);
 	if (r->cursor.state != XLOG_CURSOR_CLOSED) {
 		/*

src/box/recovery.h
@@ -30,6 +30,7 @@
  * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  */
+#include "small/rlist.h"
 #include "trivia/util.h"
 #include "third_party/tarantool_ev.h"
 #include "xlog.h"
@@ -54,6 +55,8 @@ struct recovery {
 	 * locally or send to the replica.
 	 */
 	struct fiber *watcher;
+	/** List of triggers invoked when the current WAL is closed. */
+	struct rlist on_close_log;
 };
 
 struct recovery *

commit cf85cfeca3ada59e833861216d85c6749f6d8a1d

relay: don't delete xlog files until replica confirms receipt
We remove old xlog files as soon as we have sent them to all replicas.
However, the fact that we have successfully sent something to a replica
doesn't necessarily mean the replica will have received it. If a replica
fails to apply a row (for instance, it is out of memory), replication
will stop, but the data files have already been deleted on the master so
that when the replica is back online, the master won't find appropriate
xlog to feed to the replica and replication will stop again.

The user visible effect is the following error message in the log and in
the replica status:

  Missing .xlog file between LSN 306 {1: 306} and 311 {1: 311}

There is no way to recover from this but to re-bootstrap the replica
from scratch.

The issue was introduced by commit ba09475f5f55 ("replica: advance gc
state only when xlog is closed"), which targeted at making the status
update procedure as lightweight and fast as possible and so moved
gc_consumer_advance() from tx_status_update() to a special gc message.
A gc message is created and sent to TX as soon as an xlog is relayed.
Let's rework this so that gc messages are appended to a special queue
first and scheduled only when the relay receives the receipt
confirmation from the replica.

Closes #2825

src/box/relay.cc
@@ -74,6 +74,11 @@ struct relay_status_msg {
 struct relay_gc_msg {
 	/** Parent */
 	struct cmsg msg;
+	/**
+	 * Link in the list of pending gc messages,
+	 * see relay::pending_gc.
+	 */
+	struct stailq_entry in_pending;
 	/** Relay instance */
 	struct relay *relay;
 	/** Vclock signature to advance to */
@@ -117,6 +122,11 @@ struct relay {
 	struct cpipe relay_pipe;
 	/** Status message */
 	struct relay_status_msg status_msg;
+	/**
+	 * List of garbage collection messages awaiting
+	 * confirmation from the replica.
+	 */
+	struct stailq pending_gc;
 
 	struct {
 		/* Align to prevent false-sharing with tx thread */
@@ -137,9 +147,9 @@ relay_send_initial_join_row(struct xstream *stream, struct xrow_header *row);
 static void
 relay_send_row(struct xstream *stream, struct xrow_header *row);
 
-static inline void
-relay_init(struct relay *relay, int fd, uint64_t sync,
-	   void (*stream_write)(struct xstream *, struct xrow_header *))
+static void
+relay_create(struct relay *relay, int fd, uint64_t sync,
+	     void (*stream_write)(struct xstream *, struct xrow_header *))
 {
 	memset(relay, 0, sizeof(*relay));
 	xstream_create(&relay->stream, stream_write);
@@ -147,9 +157,25 @@ relay_init(struct relay *relay, int fd, uint64_t sync,
 	relay->sync = sync;
 	fiber_cond_create(&relay->reader_cond);
 	diag_create(&relay->diag);
+	stailq_create(&relay->pending_gc);
 }
 
-static inline void
+static void
+relay_destroy(struct relay *relay)
+{
+	struct relay_gc_msg *gc_msg, *next_gc_msg;
+	stailq_foreach_entry_safe(gc_msg, next_gc_msg,
+				  &relay->pending_gc, in_pending) {
+		free(gc_msg);
+	}
+	if (relay->r != NULL)
+		recovery_delete(relay->r);
+	fiber_cond_destroy(&relay->reader_cond);
+	diag_destroy(&relay->diag);
+	TRASH(relay);
+}
+
+static void
 relay_set_cord_name(int fd)
 {
 	char name[FIBER_NAME_MAX];
@@ -168,9 +194,10 @@ void
 relay_initial_join(int fd, uint64_t sync, struct vclock *vclock)
 {
 	struct relay relay;
-	relay_init(&relay, fd, sync, relay_send_initial_join_row);
+	relay_create(&relay, fd, sync, relay_send_initial_join_row);
 	assert(relay.stream.write != NULL);
 	engine_join_xc(vclock, &relay.stream);
+	relay_destroy(&relay);
 }
 
 int
@@ -193,7 +220,7 @@ relay_final_join(int fd, uint64_t sync, struct vclock *start_vclock,
 	         struct vclock *stop_vclock)
 {
 	struct relay relay;
-	relay_init(&relay, fd, sync, relay_send_row);
+	relay_create(&relay, fd, sync, relay_send_row);
 	relay.r = recovery_new(cfg_gets("wal_dir"),
 			       cfg_geti("force_recovery"),
 			       start_vclock);
@@ -204,7 +231,7 @@ relay_final_join(int fd, uint64_t sync, struct vclock *start_vclock,
 	if (rc == 0)
 		rc = cord_cojoin(&relay.cord);
 
-	recovery_delete(relay.r);
+	relay_destroy(&relay);
 
 	if (rc != 0)
 		diag_raise();
@@ -266,7 +293,35 @@ relay_on_close_log_f(struct trigger *trigger, void * /* event */)
 	cmsg_init(&m->msg, route);
 	m->relay = relay;
 	m->signature = vclock_sum(&relay->r->vclock);
-	cpipe_push(&relay->tx_pipe, &m->msg);
+	/*
+	 * Do not invoke garbage collection until the replica
+	 * confirms that it has received data stored in the
+	 * sent xlog.
+	 */
+	stailq_add_tail_entry(&relay->pending_gc, m, in_pending);
+}
+
+/**
+ * Invoke pending garbage collection requests.
+ *
+ * This function schedules the most recent gc message whose
+ * signature is less than or equal to the given one. Older
+ * messages are discarded as their job will be done by the
+ * scheduled message anyway.
+ */
+static inline void
+relay_schedule_pending_gc(struct relay *relay, int64_t signature)
+{
+	struct relay_gc_msg *curr, *next, *gc_msg = NULL;
+	stailq_foreach_entry_safe(curr, next, &relay->pending_gc, in_pending) {
+		if (curr->signature > signature)
+			break;
+		stailq_shift(&relay->pending_gc);
+		free(gc_msg);
+		gc_msg = curr;
+	}
+	if (gc_msg != NULL)
+		cpipe_push(&relay->tx_pipe, &gc_msg->msg);
 }
 
 static void
@@ -401,6 +456,8 @@ relay_subscribe_f(va_list ap)
 		vclock_copy(&relay->status_msg.vclock, send_vclock);
 		relay->status_msg.relay = relay;
 		cpipe_push(&relay->tx_pipe, &relay->status_msg.msg);
+		/* Collect xlog files received by the replica. */
+		relay_schedule_pending_gc(relay, vclock_sum(send_vclock));
 	}
 
 	say_crit("exiting the relay loop");
@@ -450,7 +507,7 @@ relay_subscribe(int fd, uint64_t sync, struct replica *replica,
 	}
 
 	struct relay relay;
-	relay_init(&relay, fd, sync, relay_send_row);
+	relay_create(&relay, fd, sync, relay_send_row);
 	relay.r = recovery_new(cfg_gets("wal_dir"),
 			       cfg_geti("force_recovery"),
 			       replica_clock);
@@ -465,10 +522,8 @@ relay_subscribe(int fd, uint64_t sync, struct replica *replica,
 		rc = cord_cojoin(&relay.cord);
 
 	replica_clear_relay(replica);
-	recovery_delete(relay.r);
+	relay_destroy(&relay);
 
-	fiber_cond_destroy(&relay.reader_cond);
-	diag_destroy(&relay.diag);
 	if (rc != 0)
 		diag_raise();
 }

test/replication/gc.result
@@ -163,6 +163,105 @@ fiber.sleep(0.1) -- wait for relay to notify tx
 ---
 - true
 ...
+--
+-- Check that the master doesn't delete xlog files sent to the
+-- replica until it receives a confirmation that the data has
+-- been applied (gh-2825).
+--
+test_run:cmd("switch replica")
+---
+- true
+...
+-- Prevent the replica from applying any rows.
+box.error.injection.set("ERRINJ_WAL_DELAY", true)
+---
+- ok
+...
+test_run:cmd("switch default")
+---
+- true
+...
+-- Generate some data on the master.
+for i = 1, 5 do s:auto_increment{} end
+---
+...
+box.snapshot() -- rotate xlog
+---
+- ok
+...
+for i = 1, 5 do s:auto_increment{} end
+---
+...
+fiber.sleep(0.1) -- wait for master to relay data
+---
+...
+-- Garbage collection must not delete the old xlog file
+-- (and the corresponding snapshot), because it is still
+-- needed by the replica.
+#box.internal.gc.info().checkpoints == 2 or box.internal.gc.info()
+---
+- true
+...
+test_run:cmd("switch replica")
+---
+- true
+...
+-- Unblock the replica and make it fail to apply a row.
+box.info.replication[1].upstream.message == nil
+---
+- true
+...
+box.error.injection.set("ERRINJ_WAL_WRITE", true)
+---
+- ok
+...
+box.error.injection.set("ERRINJ_WAL_DELAY", false)
+---
+- ok
+...
+while box.info.replication[1].upstream.message == nil do fiber.sleep(0.01) end
+---
+...
+box.info.replication[1].upstream.message
+---
+- Failed to write to disk
+...
+test_run:cmd("switch default")
+---
+- true
+...
+-- Restart the replica to reestablish replication.
+test_run:cmd("restart server replica")
+---
+- true
+...
+-- Wait for the replica to catch up.
+test_run:cmd("switch replica")
+---
+- true
+...
+fiber = require('fiber')
+---
+...
+while box.space.test:count() < 310 do fiber.sleep(0.01) end
+---
+...
+box.space.test:count()
+---
+- 310
+...
+test_run:cmd("switch default")
+---
+- true
+...
+-- Now it's safe to drop the old xlog.
+fiber.sleep(0.1) -- wait for relay to notify tx
+---
+...
+#box.internal.gc.info().checkpoints == 1 or box.internal.gc.info()
+---
+- true
+...
 -- Stop the replica.
 test_run:cmd("stop server replica")
 ---

test/replication/gc.test.lua
@@ -86,6 +86,44 @@ test_run:cmd("switch default")
 fiber.sleep(0.1) -- wait for relay to notify tx
 #box.internal.gc.info().checkpoints == 1 or box.internal.gc.info()
 
+--
+-- Check that the master doesn't delete xlog files sent to the
+-- replica until it receives a confirmation that the data has
+-- been applied (gh-2825).
+--
+test_run:cmd("switch replica")
+-- Prevent the replica from applying any rows.
+box.error.injection.set("ERRINJ_WAL_DELAY", true)
+test_run:cmd("switch default")
+-- Generate some data on the master.
+for i = 1, 5 do s:auto_increment{} end
+box.snapshot() -- rotate xlog
+for i = 1, 5 do s:auto_increment{} end
+fiber.sleep(0.1) -- wait for master to relay data
+-- Garbage collection must not delete the old xlog file
+-- (and the corresponding snapshot), because it is still
+-- needed by the replica.
+#box.internal.gc.info().checkpoints == 2 or box.internal.gc.info()
+test_run:cmd("switch replica")
+-- Unblock the replica and make it fail to apply a row.
+box.info.replication[1].upstream.message == nil
+box.error.injection.set("ERRINJ_WAL_WRITE", true)
+box.error.injection.set("ERRINJ_WAL_DELAY", false)
+while box.info.replication[1].upstream.message == nil do fiber.sleep(0.01) end
+box.info.replication[1].upstream.message
+test_run:cmd("switch default")
+-- Restart the replica to reestablish replication.
+test_run:cmd("restart server replica")
+-- Wait for the replica to catch up.
+test_run:cmd("switch replica")
+fiber = require('fiber')
+while box.space.test:count() < 310 do fiber.sleep(0.01) end
+box.space.test:count()
+test_run:cmd("switch default")
+-- Now it's safe to drop the old xlog.
+fiber.sleep(0.1) -- wait for relay to notify tx
+#box.internal.gc.info().checkpoints == 1 or box.internal.gc.info()
+
 -- Stop the replica.
 test_run:cmd("stop server replica")
 test_run:cmd("cleanup server replica")